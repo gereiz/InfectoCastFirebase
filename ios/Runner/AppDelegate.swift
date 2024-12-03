706437e5858dced7ee015c74deeb53448da4aa4a
gereiz <georgie.reis@outlook.com>
2024-12-03 14:58:54 -0300

    Realizada implantação com Jivochat

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -4,6 +4,10 @@ import Flutter
 
 import FBSDKCoreKit
 
+import UserNotifications
+
+import JivoSDK
+
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
@@ -17,4 +21,36 @@ import FBSDKCoreKit
     )
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+
+
+override func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        Jivo.notifications.setPushToken(data: deviceToken)
+    }
+
+    override func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        Jivo.notifications.setPushToken(data: nil)
+    }
+
+    override func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        if let result = Jivo.notifications.didReceiveRemoteNotification(userInfo: userInfo) {
+            completionHandler(result)
+        }
+        else {
+            completionHandler(.noData)
+        }
+    }
+
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        if let options = Jivo.notifications.willPresent(notification: notification, preferableOptions: .banner) {
+            completionHandler(options)
+        }
+        else {
+            completionHandler([])
+        }
+    }
+
+    override func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        Jivo.notifications.didReceive(response: response)
+        completionHandler()
+    }
 }
