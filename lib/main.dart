54201c5cb721baf94157ff4a23ba48179de31c3c
gereiz <georgie.reis@outlook.com>
2025-01-13 16:45:29 -0300

    Remoção Jivo Chat

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter/material.dart';
 
@@ -17,9 +16,9 @@ import 'package:floating_bottom_navigation_bar/floating_bottom_navigation_bar.da
 import 'index.dart';
 import 'flutter_flow/revenue_cat_util.dart' as revenue_cat;
 import 'backend/stripe/payment_manager.dart';
+
 import 'package:flutter_meta_sdk/flutter_meta_sdk.dart';
-import 'package:jivosdk_plugin/bridge.dart';
-import 'package:jivosdk_plugin/plugin.dart';
+// import 'package:jivosdk_plugin/bridge.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -29,9 +28,6 @@ void main() async {
   await initFirebase();
 
   await SQLiteManager.initialize();
-
-  await Firebase.initializeApp();
-  Jivo.session.setup(channelId: 'vjrJVxvm1Z', userToken: 'userToken');
 
   final appState = FFAppState(); // Initialize FFAppState
   await appState.initializePersistedState();
@@ -68,7 +64,6 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   ThemeMode _themeMode = ThemeMode.system;
 
-
   late AppStateNotifier _appStateNotifier;
   late GoRouter _router;
 
@@ -92,7 +87,7 @@ class _MyAppState extends State<MyApp> {
     jwtTokenStream.listen((_) {});
     Future.delayed(
       const Duration(milliseconds: 1000),
-          () => _appStateNotifier.stopShowingSplashImage(),
+      () => _appStateNotifier.stopShowingSplashImage(),
     );
   }
 
@@ -104,8 +99,8 @@ class _MyAppState extends State<MyApp> {
   }
 
   void setThemeMode(ThemeMode mode) => safeSetState(() {
-    _themeMode = mode;
-  });
+        _themeMode = mode;
+      });
 
   @override
   Widget build(BuildContext context) {
@@ -168,12 +163,16 @@ class _NavBarPageState extends State<NavBarPage> {
               .removeViewPadding(removeBottom: true),
           child: _currentPage ?? tabs[_currentPageName]!),
       extendBody: true,
-      floatingActionButton: FloatingActionButton(
-        onPressed: _openJivoSDK,
-        tooltip: 'Open chat JivoSDK',
-        child: const Icon(Icons.chat_bubble),
-        backgroundColor: const Color(0xFFFCAF23),
-      ),
+
+      // floatingActionButton: valueOrDefault(currentUserDocument?.gold, 0) == 1
+      //     ? FloatingActionButton(
+      //   onPressed: _openJivoSDK,
+      //   tooltip: 'Open chat JivoSDK',
+      //   child: const Icon(Icons.chat_bubble),
+      //   backgroundColor: const Color(0xFFFCAF23),
+      // )
+      //     : null,
+
       bottomNavigationBar: FloatingNavbar(
         currentIndex: currentIndex,
         onTap: (i) => safeSetState(() {
@@ -198,7 +197,7 @@ class _NavBarPageState extends State<NavBarPage> {
                 Icon(
                   Icons.home_outlined,
                   color:
-                  currentIndex == 0 ? const Color(0xFFF4F4F4) : const Color(0x8A000000),
+                      currentIndex == 0 ? const Color(0xFFF4F4F4) : const Color(0x8A000000),
                   size: 24.0,
                 ),
                 Text(
@@ -221,7 +220,7 @@ class _NavBarPageState extends State<NavBarPage> {
                 Icon(
                   Icons.newspaper,
                   color:
-                  currentIndex == 1 ? const Color(0xFFF4F4F4) : const Color(0x8A000000),
+                      currentIndex == 1 ? const Color(0xFFF4F4F4) : const Color(0x8A000000),
                   size: 24.0,
                 ),
                 Text(
@@ -244,7 +243,7 @@ class _NavBarPageState extends State<NavBarPage> {
                 Icon(
                   Icons.queue_music_sharp,
                   color:
-                  currentIndex == 2 ? const Color(0xFFF4F4F4) : const Color(0x8A000000),
+                      currentIndex == 2 ? const Color(0xFFF4F4F4) : const Color(0x8A000000),
                   size: 24.0,
                 ),
                 Text(
@@ -267,7 +266,7 @@ class _NavBarPageState extends State<NavBarPage> {
                 Icon(
                   Icons.calculate_outlined,
                   color:
-                  currentIndex == 3 ? const Color(0xFFF4F4F4) : const Color(0x8A000000),
+                      currentIndex == 3 ? const Color(0xFFF4F4F4) : const Color(0x8A000000),
                   size: 24.0,
                 ),
                 Text(
@@ -286,9 +285,5 @@ class _NavBarPageState extends State<NavBarPage> {
         ],
       ),
     );
-  }
-
-  void _openJivoSDK() {
-    Jivo.display.present();
   }
 }
